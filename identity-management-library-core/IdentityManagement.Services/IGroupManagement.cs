92a2deb5f03e05801bb3d5c60a30431d3895f35b
E. Spangler <ernest.spangler@gmail.com>
2021-10-01 09:17:35 -0400

    v1.3.0 release 1
    Azure (IDM) identity management services - v1.3.0 release 1

--- identity-management-library-core/IdentityManagement.Services/IGroupManagement.cs
+++ identity-management-library-core/IdentityManagement.Services/IGroupManagement.cs
@@ -1,7 +1,7 @@
 ﻿//
 //  IGroupManagement.cs
 //
-//  Copyright (c) Wiregrass Code Technology 2020-2021
+//  Wiregrass Code Technology 2020-2021
 //
 using System.Collections.Generic;
 using System.Threading.Tasks;
